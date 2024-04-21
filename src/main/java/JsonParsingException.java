cd2791fb3bb9fbb004835edd71ae2760a234d177
Bartek <bartosz_janusz731@wp.pl>
2024-04-21 19:09:35 +0200

    Final version

--- src/main/java/JsonParsingException.java
+++ src/main/java/JsonParsingException.java
@@ -1,3 +1,6 @@
+/**
+ * Exception thrown when there is an error parsing JSON.
+ */
 public class JsonParsingException extends RuntimeException {
     public JsonParsingException(String message, Throwable cause) {
         super(message, cause);
